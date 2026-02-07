3849441baa210ba1e0142ead4f15969c4fdc9969
jaman hasan <jamanhasan246@gmail.com>
2026-02-08 01:07:50 +0600

    fixed

--- chathub-backend/src/utils/socket.ts
+++ chathub-backend/src/utils/socket.ts
@@ -4,11 +4,11 @@ import { Server } from 'socket.io'
 const app = express()
 const server = createServer(app)
 
-
 const io = new Server(server, {
+  transports: ['websocket'],
   cors: {
-    origin: "https://chathub-frontend-beta.vercel.app",
-    credentials: true
-  }
-});
+    origin: 'https://chathub-frontend-beta.vercel.app',
+    credentials: true,
+  },
+})
 export { app, server, io }
--- chathub-frontend/src/api/socket.api.ts
+++ chathub-frontend/src/api/socket.api.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
 export const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ['websocket'], // 🔥 important
   withCredentials: true,
 })
 
